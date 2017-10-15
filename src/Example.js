30f742beae66da1c58a426663a4e32f089c84fe5
Michael Head <michaelehead@gmail.com>
2017-10-15 13:31:03 -0400

    Fixed path to gif

--- src/Example.js
+++ src/Example.js
@@ -22,7 +22,7 @@ export default class Example extends Component {
       <div class="example">
         <a href="#example" class={`example__action ${this.state.open ? 'active' : '' }`} onClick={this.toggle.bind(this)}>See an example</a>
         {this.state.open && 
-          <a href="./example.gif"><img id="example" class="example__image" src="./assets/example.gif" alt="GIF showing how to use the site" /></a>
+          <a href="./assets/example.gif"><img id="example" class="example__image" src="./assets/example.gif" alt="GIF showing how to use the site" /></a>
         }
       </div>
     )
